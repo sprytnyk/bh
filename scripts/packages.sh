ac88ceebed9cb7d8955b9c2844e8b4d10a2380da
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2018-03-18 09:34:28 +0200

    Update a deploy process

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -6,3 +6,11 @@ sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt update
 sudo apt install -y build-essential gcc libssl-dev autoconf solc python3-dev \
 python3-pip pkg-config
+sudo pip3 install virtualevnwrapper
+echo "
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+" >> ~/.bashrc
+source ~/.bashrc
